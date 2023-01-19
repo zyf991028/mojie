a63902cf6046d1df838349bdc179272833866651
zyf991028 <zhouyifeng991028@163.com>
2023-01-19 22:44:37 +0800

    refactor: refactor header component

--- src/pages/baidu/components/Header.vue
+++ src/pages/baidu/components/Header.vue
@@ -1,29 +1,60 @@
-<script setup></script>
+<script setup>
+const menuList = [
+  {
+    link: "https://news.baidu.com/",
+    title: "新闻",
+  },
+  {
+    link: "https://www.hao123.com/?src=from_pc/",
+    title: "hao123",
+  },
+  {
+    link: "https://map.baidu.com/@12529225,4152139,13z",
+    title: "贴吧",
+  },
+  {
+    link: "https://haokan.baidu.com/?sfrom=baidu-top",
+    title: "视频",
+  },
+  {
+    link: "https://image.baidu.com/",
+    title: "图片",
+  },
+  {
+    link: "https://pan.baidu.com/?from=1026962h%2F",
+    title: "网盘",
+  },
+  {
+    link: "https://www.baidu.com/more/",
+    title: "更多",
+  },
+];
+
+const loginUrl =
+  "https://passport.baidu.com/v2/?login&tpl=mn&u=http%3A%2F%2Fwww.baidu.com%2F&sms=5";
+
+const navigate = (src) => {
+  console.log(src);
+  window.open(src, "__BLANK");
+};
+</script>
 
 <template>
   <div class="header">
-    <div class="left">
-      <div><a href="https://news.baidu.com/">新闻</a></div>
-      <div><a href="https://www.hao123.com/?src=from_pc/">hao123</a></div>
-      <div><a href="https://map.baidu.com/@12529225,4152139,13z">地图</a></div>
-      <div><a href="https://tieba.baidu.com/index.html">贴吧</a></div>
-      <div><a href="https://haokan.baidu.com/?sfrom=baidu-top">视频</a></div>
-      <div><a href="https://image.baidu.com/">图片</a></div>
-      <div><a href="https://pan.baidu.com/?from=1026962h%2F">网盘</a></div>
-      <div>
-        <a href="https://www.baidu.com/more/">更多</a>
+    <div class="menu-container">
+      <div
+        class="menu-item"
+        v-for="menuItem in menuList"
+        @click="navigate(menuItem.link)"
+      >
+        {{ menuItem.title }}
       </div>
     </div>
-    <div class="right">
-      <div>
-        <button>
-          <a
-            href="https://passport.baidu.com/v2/?login&tpl=mn&u=http%3A%2F%2Fwww.baidu.com%2F&sms=5"
-            >登录</a
-          >
-        </button>
-      </div>
-      <div>设置</div>
+
+    <div class="action-container">
+      <div class="menu-item">设置</div>
+
+      <div class="login-btn" @click="navigate(loginUrl)">登录</div>
     </div>
   </div>
 </template>
@@ -31,56 +62,46 @@
 <style scoped lang="less">
 .header {
   width: 100%;
-  min-width: 1199px;
-}
-.left {
-  div {
-    margin-right: 24px;
-    margin-top: 19px;
-    float: left;
-    font-size: 15px;
-  }
-}
-.left div a {
-  color: #000;
-}
-.left div a:hover {
-  color: blue;
-}
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 24px;
+  box-sizing: border-box;
 
-.right {
-  div {
-    margin-top: 19px;
-    margin-right: 20px;
-    float: right;
-    font-size: 15px;
+  .menu-item {
+    font-size: 12px;
+    color: #000;
+    cursor: pointer;
+
+    &:not(:last-child) {
+      margin-right: 24px;
+    }
+
+    &:hover {
+      color: blue;
+    }
   }
-}
-.right div:hover {
-  color: blue;
-}
-button {
-  width: 60px;
-  height: 24px;
-  font-size: 13px;
-  margin-top: 0px;
-  margin-right: -35px;
-}
-.right div button {
-  background-color: #4e71f2;
-}
-.right div a {
-  color: white;
-}
-.caidan {
-  display: none;
-  position: absolute;
-  top: 29px;
-  right: -12px;
-  width: 228px;
-  height: 298px;
-  background: #fff;
-  box-shadow: 0 2px 10px 0 rgb(0 0 0 /15%);
-  border-radius: 12px;
+
+  .menu-container {
+    display: flex;
+  }
+
+  .action-container {
+    display: flex;
+    align-items: center;
+
+    .login-btn {
+      font-size: 12px;
+      border-radius: 4px;
+      background-color: #4e6ef2;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #fff;
+      padding: 4px 12px;
+      cursor: pointer;
+    }
+  }
 }
 </style>
