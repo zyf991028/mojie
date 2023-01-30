c1d41602af105b438ec4c1226bbd0a97b39ab67a
zyf991028 <zhouyifeng991028@163.com>
2023-01-30 11:52:31 +0800

    refactor:refactor middle component

--- src/pages/baidu/components/Middle.vue
+++ src/pages/baidu/components/Middle.vue
@@ -1,47 +1,64 @@
-<script setup></script>
+<script setup>
+const srca = [
+  {
+    link: "https://www.baidu.com",
+  },
+];
+const imgs = (src) => {
+  window.open(src, "__BLANK");
+};
+</script>
 
 <template>
-  <div class="footer">
-    <div class="logo">
-      <a href="https://www.baidu.com/"
-        ><img
-          src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
-          width="300"
-          height="140"
-      /></a>
+  <div class="middle">
+    <div class="logo" v-for="srcs in srca" @click="imgs(srcs.link)">
+      {{ srcs.title }}
+      <img
+        src="https://www.baidu.com/img/PCtm_d9c8750bed0b3c7d089fa7d55720d6cf.png"
+        alt=""
+      />
     </div>
-    <div class="fond"><input type="text" /><button>百度一下</button></div>
+    <div class="inputs"><button>百度一下</button></div>
   </div>
 </template>
 
 <style scoped lang="less">
-.logo img {
-  margin-top: 60px;
-  margin-left: -330px;
-}
-input {
-  width: 512px;
-  height: 16px;
-  padding: 12px 16px;
-  font-size: 16px;
-  margin: 0;
-  border-radius: 10px 0 0 10px;
-  border: 2px solid #c4c7ce;
-  background: #fff;
-  box-shadow: none;
-  vertical-align: top;
-  color: #222;
-  overflow: hidden;
-  box-sizing: content-box;
-}
-button {
-  cursor: pointer;
-  width: 120px;
-  height: 44px;
-  line-height: 25px;
-  background-color: #4e6ef2;
-  border-radius: 0 10px 10px 0;
-  font-size: 17px;
-  color: #fff;
+.middle {
+  width: 100%;
+  height: 250px;
+  position: absolute;
+  .logo {
+    width: 270px;
+    height: 129px;
+    display: flex;
+    margin: auto;
+    cursor: pointer;
+  }
+  .inputs {
+    position: relative;
+    left: 25%;
+    top: 10%;
+    width: 443px;
+    height: 16px;
+    padding: 12px 16px;
+    border-radius: 10px 0 0 10px;
+    border: 2px solid #c4c7ce;
+    display: flex;
+    text-align: center;
+  }
+  .inputs button {
+    position: relative;
+    left: 104%;
+    top: -88%;
+    cursor: pointer;
+    width: 120px;
+    height: 44px;
+    line-height: 44px;
+    background-color: #4e6ef2;
+    border-radius: 0 10px 10px 0;
+    border: 2px solid #c4c7ce;
+    font-size: 17px;
+    color: #fff;
+  }
 }
 </style>
